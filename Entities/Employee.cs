adfb004cfb569f43c8c36298b1d1dd1e7d9239e2
Manoela Moreno Frattini <84809830+manoela27@users.noreply.github.com>
2025-04-22 17:24:21 -0300

    Add Entities/Employee.cs 1745353463838

---
+++ Entities/Employee.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SupremoSchedulingSystem.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        // Navigation properties
+        public ICollection<Appointment>? Appointments { get; set; }
+    }
+}
